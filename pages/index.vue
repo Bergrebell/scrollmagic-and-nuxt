2655d2f5e7cd5afec25499895ad3d1cd5edb6506
Roman Kuepper <rrkuepper@gmail.com>
2019-08-07 10:32:22 +0200

    adds vue-scrollmagic example

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,63 @@
 <template>
   <div class="container">
-    <div :ref="`myDiv`" @mouseover="animateDiv">
-      FOO
+    <div id="block">
+      Hi there !
     </div>
   </div>
 </template>
 
 <script>
-import TweenMax from 'gsap/umd/TweenMax'
+// eslint-disable-next-line no-unused-vars
+import ScrollToPlugin, { TweenMax } from 'gsap/umd/TweenMax'
 
 export default {
-  methods: {
-    animateDiv() {
-      const myDiv = this.$refs.myDiv
-      TweenMax.killTweensOf(myDiv)
-      TweenMax.to(myDiv, 0.5, { scaleX: -1 })
-    }
+  mounted() {
+    const scene2 = this.$scrollmagic
+      .scene({
+        // ID of element where animation starts
+        triggerElement: '.container',
+
+        // {0,0.5,1} - animations starts from {top,center,end} of window
+        triggerHook: 0.5,
+
+        // Duration of animation
+        duration: 300
+      })
+
+      // Declaration of animation and attaching to element
+      .setTween('#block', {
+        css: {
+          backgroundColor: 'blue'
+        },
+        scale: 0.7 // the tween durtion can be omitted and defaults to 1
+      })
+      // Helpful tags for orientation on the screen
+      .addIndicators({ name: '2 (duration: 300)' })
+
+    // Add Scene to controller
+    this.$scrollmagic.addScene(scene2)
   }
 }
 </script>
 
-<style></style>
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: aliceblue;
+}
+
+.container {
+  margin: 55vh 0;
+  padding: 50px;
+  outline: 1px dashed orange;
+}
+
+#block {
+  padding: 10px;
+  border: 1px solid black;
+  font-family: Helvetica;
+}
+</style>
